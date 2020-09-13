47f92ba0f4b0ae75bae53f58a205d27e398449a0
Abel Kalum <chumbabel@gmail.com>
2020-09-13 21:02:00 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # Add your routes here
   get '/' => 'welcome#home'
-  get '/auth/facebook/callback(.:format)' => 'sessions#create'
+  get '/auth/facebook/callback' => 'sessions#create'
 end
